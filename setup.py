27af7a7263b6da08bc7017365b8d48221fd17825
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-18 20:48:26 +0800

    update

--- setup.py
+++ setup.py
@@ -103,10 +103,10 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(exclude=["aiycsnlp", "wordcloud", "*.tests", "*.tests.*", "tests.*"]),
     # packages=find_packages(include=["aiycsnlp", "*.aiycsnlp", "*.aiycsnlp.*", "tests.*", "*tests", "*.wordcloud", "*.wordcloud.*"]),
-    packages=find_packages(
-        include=["aiycsnlp", "wordcloud"]),
+    # packages=find_packages(
+    #     include=["aiycsnlp", "wordcloud"]),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
 
