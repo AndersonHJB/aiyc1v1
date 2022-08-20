23be3c2201e0786ab18869dd0463171aaacf0304
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-20 14:12:12 +0800

    update

--- aiyc1v1/simplesearch/DataManager.py
+++ aiyc1v1/simplesearch/DataManager.py
@@ -1,20 +1,30 @@
+import pandas as pd
+
+
 class DataManager(object):
-    def __init__(self, path: str):
+    def __init__(self, path: str, ignore_postfix=[]):
         self.path = path
         self.empty_content = ""
+        self.ignore_postfix = ["zip", "png", "jpg", "jpge", "rar"]
+        self.ignore_postfix.extend(ignore_postfix)
 
     def postfix(self):
         """
         解析路径后缀，并调用指定文件读取方法
         """
         postfix = self.path.split(".")[-1]
+        if postfix in self.ignore_postfix:
+            return None
         # print(postfix)
+        # else:
         if postfix == "txt":
             self.read_file()
         elif postfix == "py":
             self.read_file()
         elif postfix == "csv":
             self.read_csv()
+        elif postfix == "xlsx" or postfix == "xls":
+            self.read_excel()
 
     def read_file(self):
         """
@@ -22,13 +32,17 @@ class DataManager(object):
         通用文件打开函数
         """
         with open(self.path, "r", encoding="utf-8") as f:
-            content = f.readlines()
-            return content, len(content)
+            self.empty_content = f.readlines()
+            # return content, len(content)
+            # return content
 
     def read_csv(self):
         """
         read csv
         """
-        import pandas as pd
-        content = pd.read_csv(self.path)
-        return content
+        self.empty_content = pd.read_csv(self.path)
+        # return content
+
+    def read_excel(self):
+        self.empty_content = pd.read_excel(self.path)
+        # return content
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = 'https://github.com/AndersonHJB/aiyc1v1'
 EMAIL = 'bornforthis@bornforthis.com'
 AUTHOR = 'Bornforthis'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
