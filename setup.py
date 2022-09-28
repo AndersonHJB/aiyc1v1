490e5b810864510f41b018fd0b67f2a9af20a595
AndersonHJB <aiyuechuang@gmail.com>
2022-09-28 12:43:14 +0800

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = 'https://github.com/AndersonHJB/aiyc1v1'
 EMAIL = 'bornforthis@bornforthis.com'
 AUTHOR = 'Bornforthis'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.23'
+VERSION = '0.0.24'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
